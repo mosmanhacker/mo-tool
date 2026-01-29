eb9fb9699057598c9cfb342c7b372564fa0fda9d
mosmanhacker <mosmanhacker@gmail.com>
2026-01-29 20:26:42 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
-command -v node >/dev/null || { echo "Install Node.js (pkg install nodejs)"; exit 1; }
+command -v node >/dev/null || { echo "Install Node.js: pkg install nodejs"; exit 1; }
+
+# تثبيت المكاتب
 if [ ! -d node_modules ]; then
   echo "Installing packages..."
   npm install telegraf express axios ua-parser-js
 fi
+
 clear
 echo "=== Mo-Tool Setup ==="
+echo "Make sure central server is running"
 node mo-tool.js
